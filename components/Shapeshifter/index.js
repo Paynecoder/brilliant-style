a2d77d03710763748ac0d327c8054d65e308e3c1
Joshua Payne <113269578+Paynecoder@users.noreply.github.com>
2023-03-01 12:03:38 -0800

    Update index.js

--- components/Shapeshifter/index.js
+++ components/Shapeshifter/index.js
@@ -2,11 +2,9 @@ import { useEffect, useState } from "react";
 import styles from "./Shapeshift.module.css";
 
 const words = ["compelling", "inspiring", "unique", "engaging"];
-const words2 = ["key", "secret", "solution"];
 
 export default function Shapeshifter() {
   const [wordIndex, setWordIndex] = useState(0);
-  const [wordIndex2, setWordIndex2] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -15,19 +13,11 @@ export default function Shapeshifter() {
     return () => clearInterval(intervalId);
   }, [wordIndex]);
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setWordIndex2((wordIndex2 + 1) % words2.length);
-    }, 1500);
-    return () => clearInterval(intervalId);
-  }, [wordIndex2]);
-
   return (
     <>
       <div className={styles.shifter}>
         <h3 className={styles.shiftertxt}>
-          Unlock the <span className={styles.word}>{words2[wordIndex2]}</span>{" "}
-          to creating <span className={styles.word}>{words[wordIndex]}</span>{" "}
+          Unlock the key to creating <span className={styles.word}>{words[wordIndex]}</span>{" "}
           designs!
         </h3>
       </div>
