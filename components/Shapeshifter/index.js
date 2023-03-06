91f02f3521f57601794bbe07885dd4069f6acf7a
Joshua Payne <payne@Joshuas-MacBook-Pro.local>
2023-03-06 15:20:48 -0800

    shapeshifter

--- components/Shapeshifter/index.js
+++ components/Shapeshifter/index.js
@@ -5,22 +5,31 @@ const words = ["compelling", "inspiring", "unique", "engaging"];
 
 export default function Shapeshifter() {
   const [wordIndex, setWordIndex] = useState(0);
+  const [nextWordIndex, setNextWordIndex] = useState(1);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setWordIndex((wordIndex + 1) % words.length);
+      // increment nextWordIndex and wrap around if necessary
+      setNextWordIndex((nextWordIndex + 1) % words.length);
+      // wait for half of the flip animation duration before updating the current word index
+      setTimeout(() => {
+        setWordIndex((wordIndex + 1) % words.length);
+      }, 250);
     }, 1500);
+    // clear interval on unmount
     return () => clearInterval(intervalId);
-  }, [wordIndex]);
+  }, [wordIndex, nextWordIndex]);
 
   return (
-    <>
-      <div className={styles.shifter}>
-        <h3 className={styles.shiftertxt}>
-          Unlock the key to creating{" "}
-          <span className={styles.word}>{words[wordIndex]}</span> designs!
-        </h3>
-      </div>
-    </>
+    <div className={styles.shifter}>
+      <h3 className={styles.shiftertxt}>
+        Unlock the key to creating{" "}
+        {/* apply the flip class to the word and change the content dynamically */}
+        <span className={`${styles.word} ${styles.flip}`} key={nextWordIndex}>
+          <span className={styles.back}>{words[wordIndex]}</span>
+        </span>{" "}
+        designs!
+      </h3>
+    </div>
   );
 }
